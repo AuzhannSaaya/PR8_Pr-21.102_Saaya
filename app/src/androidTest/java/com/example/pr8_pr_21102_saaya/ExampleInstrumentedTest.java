e5eb70ade9d4b969b83adfc514daa093fc99df70
Ayuzhanna <92370064+AuzhannSaaya@users.noreply.github.com>
2023-06-25 19:32:18 +0700

    ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/pr8_pr_21102_saaya/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/pr8_pr_21102_saaya/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.pr8_pr_21102_yadikcin_sergey;
+package com.example.pr8_pr_21102_saaya;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.pr8_pr_21102_yadikcin_sergey", appContext.getPackageName());
+        assertEquals("com.example.pr8_pr_21102_saaya", appContext.getPackageName());
     }
 }
